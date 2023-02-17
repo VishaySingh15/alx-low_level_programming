7a70b18169db494aeb301594d938cde35fad2d82
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-17 15:33:05 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'This program outputs all base 10 numbers
+ * high or lower than 5'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int count;
 
 	for (count = 0; count < 10; count++)
 	{
-		printf("%d", count);
+		printf("%d\n", count);
 	}
 
 	return (0);
