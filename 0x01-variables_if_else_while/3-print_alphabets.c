d99fdda593eb3ced77f333a92587a4c51d4739b9
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-17 15:28:16 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'This program outputs the alphabet in lower and upper case
+ * high or lower than 5'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
@@ -21,6 +29,6 @@ int main(void)
 			a++;
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
