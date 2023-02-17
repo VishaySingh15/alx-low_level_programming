460955562d683fadb85aac7e8f16f60790a1ec94
Vishay <vishay1998@gmail.com>
2023-02-17 10:58:36 -0800

    100-advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int num, n, reverse = 0, remainder, array_count=0, num_length;
+	int numbers[45];
+
+	for (num = 10; num<100; num++)
+	{
+		n = num;
+		while (n != 0)
+		{
+			remainder = n % 10;
+			reverse = reverse * 10 + remainder;
+			n /= 10;
+		}
+		if (reverse < num)
+		{
+			printf("%d, ", reverse);
+			numbers[array_count] = reverse;
+			array_count++;
+		}
+		reverse = 0;
+	}
+	
+	num_length = sizeof(numbers) / sizeof(numbers[0]);
+
+	for (num = 0; num <num_length; num++)
+	{
+		for (n = 0; n < num_length-1; n++)
+		{
+			if (numbers[n] > numbers[n+1])
+			{
+				reverse = numbers[n];
+				numbers[n] = numbers[n+1];
+				numbers [n+1] = reverse;
+			}
+		}
+	}
+
+	for (num = 0; num < num_length; num++)
+	{
+		if (numbers[num] != 0)
+		{
+			putchar((numbers[num]/10) + 48);
+			putchar((numbers[num]%10) + 48);
+			if (numbers[num] == 89)
+			{
+				return (0);
+			}
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	return (0);
+}
