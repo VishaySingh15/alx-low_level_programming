cdfb2643ef022205d98a1aaacb51d93cf06e93cb
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-17 15:17:21 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,9 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * 
+ *
  * Description: 'This program outputs whether a number is positive or negative'
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
