01869e3827f0ca5720495013148db1bfab05c7f1
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-17 15:36:24 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'This program outputs the alphabet in reverse order
+ * high or lower than 5'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int alpha;
@@ -9,6 +17,6 @@ int main(void)
 	{
 		putchar(alpha);
 	}
-
+	putchar('\n');
 	return (0);
 }
