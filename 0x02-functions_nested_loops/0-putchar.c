daac250d4c328b5c4205c6de60f2e8b5c42b1c6e
Vishay <vishay1998@gmail.com>
2023-02-21 01:25:22 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* This program prints the word _putchar */
+int main(void)
+{
+	char message[] = "_putchar";
+	int count, message_len;
+
+	/* your code goes there */
+	message_len = sizeof(message)/sizeof(message[0]);
+	for (count = 0; count < message_len; count++)
+	{
+		putchar(message[count]);
+	}
+	return (0);
+}
