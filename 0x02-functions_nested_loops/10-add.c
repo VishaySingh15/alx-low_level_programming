bcb7b338cccf3001c080ae86d72c9bf6a8947966
Vishay <vishay1998@gmail.com>
2023-02-21 08:04:55 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - returns the sum of 2 integers
+ *
+ * @num1: integer
+ * @num2: integer
+ *
+ * Return: sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
