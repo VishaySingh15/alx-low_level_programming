6af2c6ccfcf7f29acb3a919d9afdd94dd1569c7a
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-21 12:14:18 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,11 @@ int main(void)
 	int count, message_len;
 
 	/* your code goes there */
-	message_len = sizeof(message)/sizeof(message[0]);
+	message_len = sizeof(message) / sizeof(message[0]);
 	for (count = 0; count < message_len; count++)
 	{
 		_putchar(message[count]);
 	}
+	_putchar('\n');
 	return (0);
 }
