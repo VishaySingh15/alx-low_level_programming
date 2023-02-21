f4317db7cc4b9adcffabf0a24dc390ff41954095
Vishay <vishay1998@gmail.com>
2023-02-21 07:42:03 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int num = 0, mult = 0, result, dig1, dig2;
+
+	while (num <= 9)
+	{
+		while (mult <= 9)
+		{
+			result = num * mult;
+			if (result > 9)
+			{
+				dig1 = result / 10;
+				dig2 = result % 10;
+				_putchar(dig1+48);
+				_putchar(dig2+48);
+			}
+			else
+			{
+				_putchar(result+48);
+			}
+			if (mult != 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar('\n');
+			}
+			mult++;
+		}
+		num++;
+	}
+}
