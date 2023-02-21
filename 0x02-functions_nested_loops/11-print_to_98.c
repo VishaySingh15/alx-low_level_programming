a3f9674cc0943bef45aef65a67b7720026e43d5e
Vishay <vishay1998@gmail.com>
2023-02-21 10:31:42 -0800

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,86 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all numbers till 98
+ *
+ * @num: integer
+ *
+ *
+ */
+void print_to_98(int num)
+{
+	int[5] digits;
+	int arr_count, slash, mod, num_copy;
+	
+	if (num >= 98)
+	{
+		while (num >= 98)
+		{
+			arr_count = 0;
+			mod = num % 10;
+			digits[arr_count] = mod;
+			slash = num / 10;
+			while (slash > 0)
+			{
+				mod = slash % 10;
+				arr_count++;
+				digit[arr_count] = mod;
+				slash = slash / 10;
+			}
+			while (arr_count >= 0)
+			{
+				_putchar(digits[arr_count] + 48);
+				arr_count --;
+			}
+			if (num == 98)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			num--;
+		}
+	}
+	else
+	{
+		while (num <= 98)
+		{
+			arr_count = 0;
+			mod = num % 10;
+			digits[arr_count] = mod;
+			slash = num / 10;
+			while (slash > 0)
+			{
+				mod = slash % 10;
+				arr_count++;
+				digit[arr_count] = mod;
+				slash = slash / 10;
+			}
+			while (arr_count >= 0)
+			{
+				if (digits[arr_count] >= 0)
+				{
+					_putchar(digits[arr_count] + 48);
+				}
+				else
+				{
+					digits[arr_count] = digits[arr_count] * -1;
+					_putchar(digits[arr_count] + 48);
+				}
+				arr_count--;
+			}
+			if (num == 98)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+			num--;
+		}
+	}
+}
