aa3704bd97c46894db02b0eb4fe66e4e8696293b
Vishay <vishay1998@gmail.com>
2023-02-21 07:12:59 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,20 @@
+/**
+ * _abs - prints and returns the last digit of an integer
+ *
+ * @a: integer
+ *
+ * Return: last digit of the integer
+ */
+int print_last_digit(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+		a = a % 10;
+		_putchar(a+48);
+		return (a);
+	}
+	a = a % 10;
+	_putchar(a+48);
+	return (a);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * jack_bauer - prints all minutes of the day
+ */
+void jack_bauer(void)
+{
+	int hour1 = 0, hour2 = 0, hour2_lim = 9, min1 = 0, min1_lim = 5, min2 = 0, min2_lim = 9;
+
+	while (hour1 <= 2)
+	{
+		if (hour1 == 2)
+		{
+			hour2_lim = 3;
+		}
+		while (hour2 <= hour2_lim)
+		{
+			while (min1 <= min1_lim)
+			{
+				while (min2 <= min2_lim)
+				{
+					_putchar(hour1+48);
+					_putchar(hour2+48);
+					_putchar(58);
+					_putchar(min1+48);
+					_putchar(min2+48);
+					_putchar('\n');
+					min2++;
+				}
+				min1++;
+			}
+			hour2++;
+		}
+		hour1++;
+	}
+}
