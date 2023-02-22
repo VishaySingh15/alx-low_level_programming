ded000164b361861c74415423e0524864039b27e
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-22 08:50:14 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
-
+/**
+ * print_to_98 - prints all numbers till 98
+ *
+ * @num: integer
+ *
+ */
+void print_to_98(int num)
+{
+	if (num > 98)
+	{
+		while (num >= 98)
+		{
+			printf("%d", num);
+			num--;
+		}
+	}
+	else
+	{
+		while (num <= 98)
+		{
+			printf("%d", num);
+			num--;
+		}
+	}
+}
