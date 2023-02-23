2f165e7b87ebce7f63a856d9700210dc5d1d67e1
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-23 10:13:32 +0200

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal line using \
+ * @n: the number of diagonal lines to be printed
+ * Return: No Return values
+ */
+
+void print_diagonal(int n)
+{
+	int count, spaces;
+	
+	while (n > 0)
+	{
+		for (spaces = 1; spaces <= count; spaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		count++;
+	}
+}
