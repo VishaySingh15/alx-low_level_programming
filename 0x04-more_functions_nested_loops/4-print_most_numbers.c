aa2730849ee8d3e20dab849926e44f5d0f474cb1
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-23 09:47:03 +0200

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers 0 to 9 except 2 and 4
+ * Return: No Return values
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+	
+	while (c < 10)
+	{
+		if (c != 2 && c !=4)
+		{
+			_putchar(c + 48);
+		}
+	}
+	_putchar('\n');
+}
