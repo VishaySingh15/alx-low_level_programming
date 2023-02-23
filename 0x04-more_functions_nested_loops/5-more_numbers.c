39c820451ee95f79fb5fdb3c7d79ad896cd377fc
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-23 10:01:35 +0200

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14 10 times
+ * Return: No Return values
+ */
+
+void more_numbers(void)
+{
+	int c = 0, num = 0, num2 = 0, limit = 9;
+	
+	while (c < 10)
+	{
+		num2 = 0;
+		while (num2 < 2)
+		{
+			num = 0;
+			if (num2 == 1)
+			{
+				limit = 4
+			}
+			while (num <= limit)
+			{
+				if (num2 == 1)
+				{
+					_putchar(49);
+				}
+				_putchar(num);
+				num++;
+			}
+			num2++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
