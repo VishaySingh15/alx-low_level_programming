c12480336a62a9800027215b147fee3af34e4b79
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-23 10:19:02 +0200

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square using #
+ * @size: size of square
+ * Return: No Return values
+ */
+
+void print_square(int size)
+{
+	int c, count;
+	
+	c = size;
+	while (size > 0)
+	{
+		for (count = 0; count < c; count++)
+		{
+			_putchar('#');
+		}
+		size--;
+	}
+}
