c68011529c20d70f2d6bf56cb445eb664067a75c
VishaySingh15 <113976618+VishaySingh15@users.noreply.github.com>
2023-02-23 11:04:09 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,27 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * print_triangle - Prints a square using #
+ * @size: size of triangle
+ * Return: No Return values
+ */
+
+void print_triangle(int size)
+{
+	int c, count, hash = 1;
+	
+	while (size > 0)
+	{
+		c = size - 1;
+		for (count = 0; count < c; count++)
+		{
+			_putchar(' ');
+		}
+		for (count = 0; count < hash; count++)
+		{
+			_putchar('#');
+		}
+		hash++;
+		size--;
+	}
+}
