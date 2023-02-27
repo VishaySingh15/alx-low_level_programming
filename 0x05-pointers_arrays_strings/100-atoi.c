61c5c16eb5e4a084254f91ee10e2dd082fd217cc
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-02-27 20:30:11 +0200

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to a number
+ * @s: String pointer
+ * Return: no return
+ */
+
+int _atoi(char *s)
+{
+	int len = 0, val = 0, sign = 0, num_over = 0;
+
+	while (*(s + len) != 0)
+	{
+		if (num_over != 0)
+		{
+			if (*(s + len) == 45)
+			{
+				sign++;
+			}
+			else if (*(s + len) >= 48 || *(s + len) <= 57)
+			{
+				val += *(s + len) - 48
+			}
+			else if (val > 0)
+			{
+				num_over++;
+			}
+		}
+		len++;
+	}
+	if (val == 0)
+	{
+		return (0);
+	}
+	else if (sign % 2 == 0)
+	{
+		return (val);
+	}
+	else
+	{
+		return (-val)
+	}
+}
