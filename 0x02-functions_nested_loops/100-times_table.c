c3371a94cec1d4d57f0aaf80619ea5e92d1b10c7
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-02-27 21:07:42 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,40 @@
-
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void print_times_table(int n)
+{
+	int num = 0, num2 = 0, mult, result, dig;
+	if (n >= 0 && n <= 15)
+	{
+		while (num <= n)
+		{
+			while (num2 < n)
+			{
+				result == num * num2;
+				dig = result / 10;
+				if (dig > 10)
+				{
+					dig = dig / 10;
+					_putchar(dig + 48);
+					dig = dig % 10;
+					_putchar(dig + 48);
+				}
+				else
+				{
+					_putchar(dig + 48);
+				}
+				dig = result / 10;
+				dig = dig % 10;
+				_putchar(dig + 48);
+				num2++;
+			}
+			num++;
+			if (num != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
