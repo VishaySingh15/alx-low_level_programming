dac7e3acbe8351719c53b9f102b3ee9ea796836b
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-02-27 19:10:28 +0200

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints ever other letter
+ * @s: String pointer
+ * Return: no return
+ */
+
+void puts2(char *s)
+{
+	int len = 0, count = 0;
+
+	while (*(s + len) != 0)
+	{
+		len++;
+	}
+}
