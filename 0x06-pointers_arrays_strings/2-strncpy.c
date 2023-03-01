5e09cb8cace3413bf3defd25a5a9fb40a31834ab
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-01 10:34:04 +0200

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies n characters from src to dest
+ * @dest: String pointer for destination
+ * @src: String pointer for source
+ * @n: Number of characters to append from src
+ * Return: dest - string pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src, int n)
+{
+	int len_dest = 0, len_src = 0;
+
+	while (*(src + len_src) != '\n' && len_src < n)
+	{
+		*(dest + len_src) = *(src + len_src);
+		len_src++;
+	}
+	if (*(src + len_src) != '\n')
+	{
+		*(dest + len_src) = 0;
+	}
+	return (dest);
+}
