6d9c0ff8521966f3edcb4040aeef0cfe56ddad5e
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-01 11:03:30 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: int pointer for array
+ * @n: array length
+ * Return: no return
+ */
+
+void reverse_array(int *a, int n)
+{
+	int n_half = 0, temp;
+
+	while (n_half < n / 2 )
+	{
+		temp = *(a + n_half)
+		*(a + n_half) = *(a + n - n_half - 1);
+		*(a + n - n_half - 1) = temp;
+		n_half++;
+	}
+}
