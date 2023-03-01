823eb9a49358d1c706ec685790e53c20cd2bb1e4
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-01 11:46:20 +0200

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies n characters from src to dest
+ * @dest: String pointer for destination
+ * @src: String pointer for source
+ * @n: Number of characters to append from src
+ * Return: dest - string pointer to destination
+ */
+
+char *leet(char *s)
+{
+	int len_s = 0, count;
+	char encode[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replace[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+
+	while (*(s + len_s) != 0)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (*(s + len_s) == encode[i])
+			{
+				*(s + len_s) = replace[i] + 48;
+			}
+		}
+		len_s++;
+	}
+	return (s);
+}
