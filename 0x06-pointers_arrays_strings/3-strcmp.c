5c561566dc8e2c7d557a7c8fa458ea73ad13cbb1
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-01 10:47:56 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncmp - Copies n characters from src to dest
+ * @s1: String 1 pointer
+ * @src: String 2 pointer
+ * Return: -15 - for s1 > s2, 15 for s2 > s1, 0 for s1 = s2
+ */
+
+char *_strcmp(char *s1, char *s2)
+{
+	int len_s1 = 0, len_s2 = 0;
+
+	while (*(s1 + len_s1) != 0)
+	{
+		len_s1++;
+	}
+	while (*(s2 + len_s2) != 0)
+	{
+		len_s2++;
+	}
+	if (len_s1 > len_s2)
+	{
+		return (-15);
+	}
+	else if (len_s1 < len_s2)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
+}
