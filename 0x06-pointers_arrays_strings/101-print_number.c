08738bdc1a22c574b8ff73d9d3aa4e2df2dc9c51
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-02 08:21:57 +0200

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337
+ * @s: String pointer
+ * Return: S - Encoded string
+ */
+
+void print_number(int n)
+{
+	int negative = 0, rem, dig, rev = 0;
+	
+	if (n < 0)
+	{
+		n = n * -1;
+		negative = 1;
+	}
+	while (dig > 0)
+	{
+		rem = n % 10;
+		rev = rev * 10 + rem;
+		dig = dig / 10;
+	}
+	if (negative > 0)
+	{
+		_putchar('-');
+	}
+	while (dig > 0)
+	{
+		rem = rev % 10;
+		_putchar(rem + 48);
+		dig = dig / 10;
+	}
+}
