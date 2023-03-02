270bca9b38ff865562140b6c45f930560c6d3679
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-02 08:25:47 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	int negative = 0, rem, dig, rev = 0;
+	int negative = 0, rem, dig = 10, rev = 0;
 	
 	if (n < 0)
 	{
@@ -19,7 +19,7 @@ void print_number(int n)
 	{
 		rem = n % 10;
 		rev = rev * 10 + rem;
-		dig = dig / 10;
+		dig = rev / 10;
 	}
 	if (negative > 0)
 	{
@@ -29,6 +29,6 @@ void print_number(int n)
 	{
 		rem = rev % 10;
 		_putchar(rem + 48);
-		dig = dig / 10;
+		dig = rev / 10;
 	}
 }
