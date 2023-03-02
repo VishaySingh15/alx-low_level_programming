1f318abcc4549353ce9f81d9c508b46a82a25c42
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-02 08:11:39 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,14 @@
 char *rot13(char *s)
 {
 	int count = 0, i;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (*(s + count) == alpha[i])
 			{
 				*(s + count) = rot13[i];
 				break;
