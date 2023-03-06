d0c22c75955235c38778f4b702396ace9f14a1ab
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 11:10:15 +0200

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - Copies n bytes from src to dest
+ * @s: String pointer for string
+ * @c: char to find
+ * Return: dest - string pointer to source
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count, count1 = 0;
+
+	while (*(accept + count1) != 0)
+	{
+		count = 0;
+		while (*(s + count) != 0)
+		{
+			if (*(s + count) == *(accept + count1))
+			{
+				return (count);
+			}
+			count++;
+		}
+		count1++;
+	}
+	return (0);
+}
