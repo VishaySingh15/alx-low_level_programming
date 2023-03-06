6596abb27a430d143cec48c5f10d4cffdbef7597
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 11:34:57 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,18 +11,18 @@ char *_strpbrk(char *s, char *accept)
 {
 	int count, count1 = 0;
 
-	while (*(accept + count1) != 0)
+	while (*(s + count) != 0)
 	{
 		count = 0;
-		while (*(s + count) != 0)
+		while (*(accept + count1) != 0)
 		{
 			if (*(s + count) == *(accept + count1))
 			{
 				return (s + count);
 			}
-			count++;
+			count1++;
 		}
-		count1++;
+		count++;
 	}
 	return (NULL);
 }
