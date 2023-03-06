5d77a33bf22065a47aa9be0bd82e91dfed31a5f4
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 10:22:10 +0200

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  * Return: dest - string pointer to source
  */
 
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int count = 0;
 	while (count < n)
