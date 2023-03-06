e5a4846ec4fcaf04182384f5f551e839dee48b26
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 11:30:25 +0200

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Checks for first occurence of byte
+ * @s: String pointer for string
+ * @accept: bytes to accept
+ * Return: s - string pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int count, count1 = 0;
+
+	while (*(accept + count1) != 0)
+	{
+		count = 0;
+		while (*(s + count) != 0)
+		{
+			if (*(s + count) == *(accept + count1))
+			{
+				return (*(s + count));
+			}
+			count++;
+		}
+		count1++;
+	}
+	return (NULL);
+}
