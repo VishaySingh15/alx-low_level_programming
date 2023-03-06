785b323cc1278efbdb8278e098405423f500873b
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 12:33:22 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,6 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int count = 0;
+
 	while (*(s + count) != 0)
 	{
 		if (*(s + count) == c)
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 		}
 		count++;
 	}
-	return (NULL);
+	return (0);
 }
