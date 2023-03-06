920c920dba86ec60ba65045615097dbc3455959a
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 13:02:37 +0200

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 			count_needle++;
 			while (*(needle + count_needle) != 0)
 			{
-				if (*needle == *(haystack + count))
+				if (*(needle+count_needle) == *(haystack + count))
 				{
 					count1++;
 				}
