c2d66f87a5842cddd92161b042f24e560d5ad6f3
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-06 09:51:26 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _memset - Fills first n bytes of memory with char b
+ * @s: String pointer for a set
+ * @b: Replacement character
+ * @n: Number of characters to replace
+ * Return: s - string pointer to set
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+	while (count < n)
+	{
+		*(s + count) = b;
+	}
+	return (s);
+}
