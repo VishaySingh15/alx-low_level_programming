1cac59cf0296dea1dee114841467f09db704d82f
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-08 09:53:54 +0200

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: String pointer
+ * Return: no return
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	count = count + _strlen_recursion(*(s + 1));
+	return (count);
+}
