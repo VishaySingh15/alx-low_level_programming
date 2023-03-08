f527895a312ab60a0fa1ec5eaba2eaa99d3c56b8
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-08 10:59:58 +0200

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string
- * @s: String pointer
- * Return: no return
+ * _pow_recursion - Calculates x to the power y
+ * @x: first integer
+ * @y: exponent
+ * Return: x pow y
  */
 
 int _pow_recursion(int x, int y)
@@ -11,6 +12,10 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 	{
 		return (1);
+	}
+	else if ()
+	{
+		return (-1)
 	}
 	else
 	{
