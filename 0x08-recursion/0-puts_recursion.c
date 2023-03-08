537c412ff2d61d09b5380e5b63d5b3266fa11f4c
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-08 09:25:09 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * _memcpy - Copies n bytes from src to dest
+ * @dest: String pointer for a destination
+ * @src: String pointer for source
+ * @n: Number of characters to replace
+ * Return: dest - string pointer to source
+ */
+
+void _puts_recursion(char *s)
+{
+	int count = 0;
+
+	if (*(s + count) == 0)
+	{
+		return;
+	}
+	_putchar(*(s + count));
+	count++;
+	_puts_recursion(*(s + count));
+}
