e1c58dfe233bcf7f458b1325686257cf2e6b9580
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-08 10:29:33 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String pointer
+ * Return: no return
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x , y - 1));
+	}
+}
