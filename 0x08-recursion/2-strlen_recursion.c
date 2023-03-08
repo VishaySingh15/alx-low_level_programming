9de3579b9991e3f28be138b13da6df983d40717c
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-08 10:07:11 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,16 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s == 0)
 	{
 		return (0);
 	}
 	else
-	{
-		count++; 
-		_strlen_recursion(s + 1);
+	{ 
+		return(1 + _strlen_recursion(s + 1));
 	}
-	return (count);
 }
