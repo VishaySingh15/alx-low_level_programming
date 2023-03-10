2d9e7c0c5a8c2b2a969f57bf491ee742573276fd
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-10 19:32:57 +0200

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: Number of arguments
+ * @argv: array of pointers for arguments
+ * Return: 0 Success
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
