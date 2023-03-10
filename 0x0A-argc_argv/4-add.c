2daa27f848a4a65e2c75415b5b4878a5a1c97f9a
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-10 20:04:05 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 	int a = 0, b = argc + 1, sum = 0;
 
 	printf("%d\n", b);
-	while (argc > 0)
+	while (argc > 1)
 	{
 		printf("%s\n", argv[b - argc]);
 		a = atoi(argv[b - argc]);
