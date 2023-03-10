2a2ef84d4c0b7621d17abfff769296dde38d34a6
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-10 19:56:25 +0200

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the product of 2 numbers
+ * @argc: Number of arguments
+ * @argv: array of pointers for arguments
+ * Return: 0 Success, 1 for Error
+ */
+
+int main(int argc, char **argv)
+{
+	int a = 0, b = argc, sum = 0;
+
+	while (argc)
+	{
+		a = atoi(argv[b - argc]);
+		if (a == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
