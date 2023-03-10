d663c59bcb103f756cc4961bbbec8205b61ea226
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-10 19:27:33 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the Number of arguments
+ * @argc: Number of arguments
+ * @argv: array of pointers for arguments
+ * Return: 0 Success
+ */
+
+int main(int argc, char **argv  __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
