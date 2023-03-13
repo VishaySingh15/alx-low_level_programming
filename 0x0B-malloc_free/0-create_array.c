098751253c68b83dd971feb54260f694064dbc49
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-13 09:40:55 +0200

    Create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of arguments
+ * @argv: array of pointers for arguments
+ * Return: 0 Success
+ */
+
+int main(int argc  __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
