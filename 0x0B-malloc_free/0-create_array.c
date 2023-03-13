848928ee62b8d732d496a0fdc041d6bfbe02ef69
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-13 09:58:35 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,22 +3,28 @@
 #include "main.h"
 
 /**
- * main - Prints the program name
- * @argc: Number of arguments
- * @argv: array of pointers for arguments
- * Return: 0 Success
+ * create_array - Creates and initiliazes an array of size n
+ * @size: Size of array
+ * @c: Initial Character
+ * Return: my_array if successful and null if not
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *my_arr;
+	int count = 0;
 
 	if (size != 0)
 	{
 		my_arr = malloc(size * sizeof(char));
 		if (my_arr != NULL)
 		{
-			my_arr [0] = c;
+			while (size)
+			{
+				my_arr[count] = c;
+				size--;
+				count++;
+			}
 			return (my_arr);
 		}
 	}
