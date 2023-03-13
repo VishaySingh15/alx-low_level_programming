b9ad1f0c92edd64d421be8274184b6d16a2a98c0
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-13 11:38:41 +0200

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - Creates and initiliazes an array of size n
+ * @size: Size of array
+ * @c: Initial Character
+ * Return: my_array if successful and null if not
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *my_arr[][];
+	int size = width * height;
+	
+	if (width > 0 && height > 0)
+	{
+		my_arr = malloc(size * sizeof(int));
+		if (my_arr != NULL)
+		{
+			while (size)
+			{
+				my_arr[count] = 0;
+				size--;
+				count+=4;
+			}
+			return (my_arr);
+		}
+	}
+	return (NULL);
+}
