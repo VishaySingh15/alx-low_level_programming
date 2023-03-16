0ebf0584686f3d6f1f19a73de55b664756a62dea
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-16 17:41:24 +0200

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Creates an array
+ * @nmemb: no of elements in array
+ * @size: Element byte size
+ * Return: my_array if successful and null if not
+ */
+
+int *array_range(int min, int max)
+{
+	char *my_arr;
+	int length, i;
+
+	if (min > max)
+		return (NULL);
+	length = max - min;
+	my_arr = malloc(length * sizeof(int));
+	if (my_arr == NULL)
+	{
+		free(my_arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+		my_arr[i] = min + i;
+
+	return (my_arr);
+}
