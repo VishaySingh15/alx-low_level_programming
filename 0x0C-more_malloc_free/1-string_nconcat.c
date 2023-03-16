e9617295eaeda1cf439b356d40e3da0206264019
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-16 17:07:14 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *my_arr;
-	int size1, size2, count = 0, count2 = 0;
+	unsigned int size1, size2, count = 0, count2 = 0;
 
 	if (s1 == NULL)
 	{
