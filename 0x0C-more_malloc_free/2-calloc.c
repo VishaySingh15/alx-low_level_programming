6ce32d4d89cdd11973aa8f91435f514e0d78d345
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-16 17:28:06 +0200

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Creates an array
+ * @nmemb: no of elements in array
+ * @height: Element byte size
+ * Return: my_array if successful and null if not
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *my_arr;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	my_arr = malloc(nmemb * size);
+	if (my_arr == NULL)
+	{
+		free(my_arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		my_arr[i] = 0;
+
+	return (my_arr);
+}
