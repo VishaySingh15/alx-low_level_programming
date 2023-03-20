1ddc2812a7a7a9c02ea79eb216aa77cee987409e
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-20 15:46:32 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- * Return: return pointer
+ * Return: return d - pointer to structure
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
