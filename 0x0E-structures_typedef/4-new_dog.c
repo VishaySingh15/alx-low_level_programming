d5fa95e8bbfb601095cd1612f50bcd88444ef4e1
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-20 16:29:46 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_copy = *name, *owner_copy = *owner;
+	char *name_copy = name, *owner_copy = owner;
 	
 	dog = malloc(sizeof(*dog));
 	if (dog == NULL)
