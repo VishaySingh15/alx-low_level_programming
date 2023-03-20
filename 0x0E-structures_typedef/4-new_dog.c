7b9afbd89db547d8cd7f341e529ba3dd32eafa91
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-20 16:22:17 +0200

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - Initializes the details of a new dog
+ * @name: Pointer to dog name
+ * @age: dog age
+ * @owner: Pointer to dog owner
+ * Return: pointer to dog structure if true, else NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *name_copy = name, *owner_copy = owner;
+	
+	dog = malloc(sizeof(dog_t));
+	if (new_dog ==NULL)
+	{
+		return (NULL);
+	}
+	dog->name = name_copy;
+	dog->age = age;
+	dog->owner = owner_copy;
+	
+	return (dog);
+}
