f9fa226ec587c07fb59692b209b913953cc61f09
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-22 14:53:03 +0200

    Create op_functions.c

---
+++ 0x0F-function_pointers/op_functions.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * op_add - adds 2 integers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - subtracts 2 integers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - multiplies 2 integers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_add - divides 2 integers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - modulo of 2 integers
+ * @a: num 1
+ * @b: num 2
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
