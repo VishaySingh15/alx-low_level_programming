8da43d393d7b77cd6a20aaea072ee5cf725ee6d3
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-22 14:55:30 +0200

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,6 @@
+#ifndef _CALC_
+#define _CALC_
+
 /**
  * struct op - Struct op
  *
@@ -9,3 +12,11 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
