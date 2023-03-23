76bd5e1180a9b139f652bbf68dc397d7bc1ae52a
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-23 15:50:30 +0200

    replaced *separator with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(arg_list, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		if (*separator)
+		if (separator)
 		{
 			printf("%d%s", va_arg(arg_list, int), separator);
 		}
