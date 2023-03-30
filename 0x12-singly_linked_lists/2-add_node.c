5de47b894630f9a4a1deb1c76e525da2934b1034
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-30 14:27:51 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,9 +10,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *nodePtr = **head;
-	list_t new_node;
+	list_t *new_node;
 	unsigned int count = 0;
-	char *str_dup = strup(str);
+	char *str_dup = strdup(str);
 
 	if (!str || !head)
 	{
@@ -22,9 +22,9 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		count ++;
 	}
-	new_node->next = nodePtr->next;
-	new_node->str = str_dup;
-	new_node->len = count;
-	nodePtr = &new_node
+	new_node.next = nodePtr->next;
+	new_node.str = str_dup;
+	new_node.len = count;
+	nodePtr = &new_node;
 	return (nodePtr);
 }
