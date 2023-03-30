4bebcf69c0329f7bfe92464f698fcf48b7bc6d12
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-30 14:20:58 +0200

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * list_len - counts the number os elements in a list
+ * @h: pointer to first node
+ * Return: number of elements
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nodePtr = **head;
+	list_t new_node;
+	unsigned int count = 0;
+	char *str_dup = strup(str);
+
+	if (!str || !head)
+	{
+		return (NULL);
+	}
+	while (*(str_dup + count) != 0)
+	{
+		count ++;
+	}
+	new_node->next = nodePtr->next;
+	new_node->str = str_dup;
+	new_node->len = count;
+	nodePtr = &new_node
+	return (nodePtr);
+}
