cb52a251dd8bd484b95412e3bc5de1f66807a4cb
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-30 15:46:59 +0200

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_next - frees nodes recursively
+ * @head: pointer to next node
+ * Return: no return
+ */
+void free_next(list_t *head)
+{
+	if (*head->next)
+	{
+		free_next(*head->next);
+	}
+	else
+	{
+		free(*head);
+	}
+}
+
+/**
+ * free_next - frees all nodes
+ * @head: pointer to first node
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	free_next(head);
+}
