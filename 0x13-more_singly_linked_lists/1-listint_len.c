4006ed7fb344e473277d2f32b52b32eee56cd670
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-03 11:07:25 +0200

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - counts number of elements in list
+ * @h: pointer to first node
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *next_ptr = h;
+	size_t num = 0;
+
+	while (next_ptr != NULL)
+	{
+		num++;
+		next_ptr = next_ptr->next;
+	}
+	return (num);
+}
