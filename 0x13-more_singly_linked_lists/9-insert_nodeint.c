d66f0fd81b30f1c7e11bb85e16f070b62f4657bd
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-04 10:09:06 +0200

    current pointer equals next

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->n = n;
 			new_node->next = current_ptr;
-			current_ptr->next = new_node;
+			current_ptr = new_node;
 			return (current_ptr);
 		}
 		current_ptr = current_ptr->next;
