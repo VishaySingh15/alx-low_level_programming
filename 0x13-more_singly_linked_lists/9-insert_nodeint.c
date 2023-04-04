3c6731cf2b2aea631ec8e4ed9231feb04a8e6b36
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-04 10:27:34 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->n = n;
 			new_node->next = current_ptr;
-			current_ptr = new_node;
+			*head = new_node;
 			return (new_node);
 		}
 		current_ptr = current_ptr->next;
