0098b8aa52f24ad76e5f502a7f5209bf6b2421be
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-11 10:27:56 +0200

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * set_bit - sets bit at position index
+ * @n: int pointer
+ * @index: bit position
+ * Return: 1 if success -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int power = 1, index_dup = index;
+
+	while (index_dup)
+	{
+		power *= 2;
+		index_dup--;
+	}
+	*n = *n - power;
+	return (1);
+}
