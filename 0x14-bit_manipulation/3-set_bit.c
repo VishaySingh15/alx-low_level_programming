7460ceef94200494e6603a33eeb0180ca363f822
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-11 10:20:23 +0200

    Create 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * set_bit - sets bit at position index
+ * @n: int pointer
+ * @index: bit position
+ * Return: 1 if success -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int count = 0, valid, power = 1, index_dup = index;
+
+	valid = *n;
+	while (valid)
+	{
+		count++;
+		valid = valid >> 1;
+	}
+	while (index_dup)
+	{
+		power *= 2;
+		index_dup--;
+	}
+	if (index < count)
+		*n = *n + power;
+		return (1);
+	return (-1);
+}
