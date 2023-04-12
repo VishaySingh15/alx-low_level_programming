31950696f3b2991803e9676f447f59584951b8b1
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-12 14:45:57 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,37 +1,57 @@
 #include "main.h"
 
 /**
- * append_text_to_file - adds text to end
- * of file
- * @filename: name of text file
- * @text_content: content to write to file
- * Return: 1 if success, -1 if failure
+ * main - copies contents from file 1
+ * to file 2
+ * @argc: number of arguments passed
+ * @argv: arrays of pointers to arguments
+ * Return: 0 success
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int my_file, letters;
-	ssize_t nwrite;
+	int file_from, file_to, letters;
+	const char *buffer = malloc(sizeof(char) * 1024);
+	ssize_t nread, nwrite;
 
-	if (!filename)
+	printf("%s%d\n", "Number of arguments :", argc);
+	printf("%s", argv[1]);
+	/*
+	if (argc != 2)
 	{
-		return (-1);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit (97);
 	}
-	my_file = open(filename, O_APPEND | O_WRONLY);
-	if (my_file < 0)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from < 0)
 	{
-		return (-1);
+		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't read from file ", argv[1]);
+		exit (98);
 	}
-	for (letters = 0; text_content[letters] != 0; letters++)
-		;
-	if (letters == 0)
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (file_to < 0)
 	{
-		text_content = "";
+		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", argv[2]);
+		exit (99);
 	}
-	nwrite = write(my_file, text_content, letters);
+	nread = read(file_from, buffer, 1024);
+	if (nread < 0)
+	{
+		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't read from file ", argv[1]);
+		exit (98);
+	}
+	nwrite = write(argv[2], buffer, nread);
 	if (nwrite < 0)
 	{
-		return (-1);
+		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", argv[2]);
+		exit (99);
 	}
-	close(my_file);
-	return (1);
+	while (nread)
+	{
+		nread = read(file_from, buffer, 1024);
+		nwrite = write(argv[2], buffer, nread);
+	}
+	close(file_from);
+	close(file_to);
+	*/
+	return (0);
 }
