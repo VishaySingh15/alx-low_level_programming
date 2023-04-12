418fe20106ac8bd83f33025bdcdc0a7f97c004eb
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-12 11:19:45 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	if (letters == 0)
 	{
-		return (1);
+		text_content = "";
 	}
 	nwrite = write(my_file, text_content, letters);
 	if (nwrite < 0)
