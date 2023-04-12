7e9e9eeb9821f2ea788763d4f4efd4de4e1ce987
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-04-12 11:17:17 +0200

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: name of text file
+ * @text_content: content to write to file
+ * Return: 1 if success, -1 if failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int my_file, letters;
+	ssize_t nwrite;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	my_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (my_file < 0)
+	{
+		return (-1);
+	}
+	for (letters = 0; text_content[letters] != 0; letters++)
+		;
+	if (letters == 0)
+	{
+		return (1);
+	}
+	nwrite = write(my_file, text_content, letters);
+	if (nwrite < 0)
+	{
+		return (-1);
+	}
+	close(my_file);
+	free(buffer);
+	return (1);
+}
